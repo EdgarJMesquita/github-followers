dc0ad99a577b196e79d801d25c1e9e4a17db3b47
EdgarJMesquita <edgaroboe@outlook.com>
2025-01-17 13:50:13 -0300

    Initial Commit

---
+++ GithubFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubFollowers
+//
+//  Created by Edgar Jonas Mesquita da Silva on 17/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
